e814090b5c3cc4638215df2dd39bdb664cc95a3f
Frederik Duchi <frederik.duchi@telenet.be>
2021-10-07 13:50:23 +0200

    implemented code for clock closing #1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 export const init = () => {
     console.log('start executing JavaSript');
+    const button = document.querySelector('.button');
+    button.addEventListener('click', () => {
+      console.log('you have clicked');
+    })
 };
